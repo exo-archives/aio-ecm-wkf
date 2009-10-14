46fb5c5363303f3f4cd1af486de124e498f47626
Minh Van Dang <minh.dang@exoplatform.com>
2009-10-14 04:37:55 +0000

    ECM-4391 fix prolem with bonita engine

--- server/jboss/patch-ear/src/main/jboss/bin/eXo.sh
+++ server/jboss/patch-ear/src/main/jboss/bin/eXo.sh
@@ -4,8 +4,8 @@ BONITA_OPTS="-Dorg.ow2.bonita.environment=../server/default/conf/bonita.environn
 #JPDA_TRANSPORT=dt_socket
 #JPDA_ADDRESS=8000
 #REMOTE_DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
-
-JAVA_OPTS="$JAVA_OPTS $BONITA_OPTS"
+LOGGING_OPTS="-Djava.util.logging.config.file=../server/default/conf/logging.properties"
+JAVA_OPTS="$JAVA_OPTS $BONITA_OPTS $LOGGING_OPTS"
 
 export JAVA_OPTS
 exec "$PRGDIR"./run.sh "$@"
--- server/jboss/patch/src/main/jboss/bin/eXo.bat
+++ server/jboss/patch/src/main/jboss/bin/eXo.bat
@@ -7,6 +7,7 @@ cd %BIN_DIR%
 
 rem Sets some variables
 set BONITA_OPTS="-Dorg.ow2.bonita.environment=..\server\default\conf\bonita.environnement.xml"
+set LOGGING_OPTS="-Djava.util.logging.config.file=..\server\default\conf\logging.properties"
 #set EXO_CONFIG_OPTS="-Dorg.exoplatform.container.configuration.debug"
 #set JPDA_TRANSPORT=dt_socket
 #set JPDA_ADDRESS=8000
